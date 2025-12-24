8e12a2e4f0a4ec83be83d40fe65635ee16b1c232
Gabriel Carbone <gabrielcarbone97@gmail.com>
2025-12-24 13:25:16 -0300

    Api URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    }
+  baseURL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
 
 export default axiosInstance;
